45279f8e44e2d1de906ecd9bf2a9f8f294b4649a
AkitotheExiled <myguestmike@gmail.com>
2019-11-06 16:22:33 -0800

    V1.02 Release
    -Added multithreading to assist with bot run time.
    -Added command variable in CONFIG.INI, the comment must equal the command.

--- post_bot.py
+++ post_bot.py
@@ -2,12 +2,15 @@ import praw
 from configparser import ConfigParser
 from datetime import datetime, timedelta
 import requests, requests.auth
+from threading import Thread
+import functools
+import time
 
 
 class Post_Bot():
 
     def __init__(self):
-        self.user_agent = "'PostLockBot- EDIT FOR r/eFreebies//V1.01 by ScoopJr'"
+        self.user_agent = "'PostLockBot- EDIT FOR r/eFreebies//V1.02 by ScoopJr'"
         print('Starting up...', self.user_agent)
         CONFIG = ConfigParser()
         CONFIG.read('config.ini')
@@ -17,6 +20,7 @@ class Post_Bot():
         self.secret = CONFIG.get('main', 'SECRET')
         self.timelimit = timedelta(weeks=int(CONFIG.get('main', 'TIMELIMIT')))
         self.subreddit = CONFIG.get('main', 'SUBREDDIT')
+        self.command = CONFIG.get('main', 'COMMAND')
         self.token_url = "https://www.reddit.com/api/v1/access_token"
         self.token = ""
         self.t_type = ""
@@ -28,7 +32,7 @@ class Post_Bot():
                              username=self.user)
         self.flair_id = CONFIG.get('main', 'FLAIR_ID')
         self.subs = {}
-        self.need_flair = []
+        self.need_flair = set()
 
     def get_token(self):
         client_auth = requests.auth.HTTPBasicAuth(self.client, self.secret)
@@ -44,20 +48,20 @@ class Post_Bot():
         for post in self.reddit.subreddit(self.subreddit).new(limit=1000):
             post_time = datetime.utcfromtimestamp(int(post.created_utc)).strftime('%Y-%m-%d %H:%M:%S')
             post_time = datetime.strptime(post_time, '%Y-%m-%d %H:%M:%S')
-            self.subs[post.id] = {'created': post_time, 'archived': post.archived, 'locked': post.locked, 'flair_id': post.link_flair_template_id}
+            self.subs[post.id] = {'created': post_time, 'archived': post.archived, 'locked': post.locked, 'flair_id': post.link_flair_template_id, 'comments': post.comments}
         return self.subs
 
-    def return_subs(self, subs):
+    def list_delta(self, subs):
         """Takes a list with a datetime object under 'created' and compares it to datetime.utcnow() and the timedelta of 1 week and returns a new list of that comparison."""
         for sub in subs:
             if ((self.check_timer - subs[sub]['created']) > self.timelimit) and (not subs[sub]['archived']) and (self.flair_id != subs[sub]['flair_id']):
                 if subs[sub]['locked']:
                     continue
                 else:
-                    self.need_flair.append(sub)
+                    self.need_flair.add(sub)
         return self.need_flair
 
-    def flair_subs(self, sub_list):
+    def flair_list(self, sub_list):
         """Simply goes through each submission and flairs them with the flair_id setup in config."""
         if not sub_list:
             return print('Your subreddit has no submissions that need to be flaired.')
@@ -65,6 +69,22 @@ class Post_Bot():
             for sub in sub_list:
                 self.reddit.submission(sub).flair.select(self.flair_id)
             return print('Done.')
+
+    def check_expired(self, sub_ids):
+        """Runs through a dictionary of post comments and returns those that need flairs"""
+        for sub in sub_ids:
+            for comment in self.subs[sub]['comments']:
+                if str(comment.body) == str(self.command):
+                    self.need_flair.add(sub)
+                elif str(comment.body) != str(self.command):
+                    continue
+        return self.need_flair
+
+    def run_bot(self):
+        list1 = self.grab_posts()
+        Thread(target = functools.partial(self.list_delta, list1)).start()
+        final_list = Thread(target = functools.partial(self.check_expired, self.need_flair)).start()
+        return self.flair_list(final_list)
 
 
 
@@ -74,6 +94,10 @@ class Post_Bot():
 
 if __name__ == "__main__":
     bot = Post_Bot()
-    list1 = bot.grab_posts()
-    list2 = bot.return_subs(list1)
-    bot.flair_subs(list2)
+    while True:
+        print('Your bot is running in the background.')
+        bot.run_bot()
+        print('Bot is done running, sleeping for 120 seconds.')
+        time.sleep(120)
+
+
